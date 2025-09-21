7c3c94c9011b9442f908dbb92d602cb3b91f02cf
MathGym <mathgymdcr@gmail.com>
2025-09-21 21:33:37 +0200

    Automatizacion

--- tests/enigma/constraints.smoke.test.js
+++ tests/enigma/constraints.smoke.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import * as mod from '../../..//plantillas/enigma_einstein.js'
+import * as mod from '../../../plantillas/enigma_einstein.js'
 
 const solver = mod.__test__
 
